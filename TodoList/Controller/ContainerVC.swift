6a98a96530a324117054c3a65f66733e5da9c994
Aminjoni Abdullozoda <aminjoniabdullozoda@Aminjonis-MacBook-Pro.local>
2018-08-18 22:41:08 +0200

    edit cell added

--- TodoList/Controller/ContainerVC.swift
+++ TodoList/Controller/ContainerVC.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ContainerViewController: UIViewController
+{
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var connectionButton: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Making button round
+        addButton.layer.cornerRadius = addButton.frame.width / 2
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ TodoList/View/ToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoCell.swift
+//  TodoList
+//
+//  Created by Aminjoni Abdullozoda on 8/18/18.
+//  Copyright © 2018 Aminjoni Abdullozoda. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
