3611c46540a24876fda7cf02ab7d736a08037837
Aminjoni Abdullozoda <aminjoniabdullozoda@Aminjonis-MacBook-Pro.local>
2018-08-18 21:37:20 +0200

    init proj

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Aminjoni Abdullozoda on 8/18/18.
+//  Copyright © 2018 Aminjoni Abdullozoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
